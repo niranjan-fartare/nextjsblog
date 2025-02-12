74aca8cb204b47e9055d0a13f0224f1b1ae57d2f
Niranjan Fartare <niranjanfartare0@gmail.com>
2025-02-12 13:15:50 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
+import GoogleAdsScript from './components/GoogleAdsScript'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -62,6 +63,7 @@ export default function RootLayout({
           <SpeedInsights />
         </main>
       </body>
+      <GoogleAdsScript />
     </html>
   )
 }
