44a53092565f3841bb53b9a869327ece6096d7d3
Niranjan Fartare <me@niranjan.co>
2025-02-12 13:34:08 +0530

    Add AdSense code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
-import GoogleAdsScript from './components/GoogleAdsScript'
+import "./global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Footer from "./components/footer";
+import { baseUrl } from "./sitemap";
+import GoogleAdsScript from "./components/GoogleAdsScript";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Niranjan Fartare | Portfolio',
-    template: '',
+    default: "Niranjan Fartare | Portfolio",
+    template: "",
   },
-  description: 'This is my portfolio.',
+  description: "This is my portfolio.",
   openGraph: {
-    title: 'Niranjan Fartare | Portfolio',
-    description: 'Upcoming SDE. Highly motivated individual and a quick learner. Currently learning Java. Aspiring to be a Full Stack Developer.',
+    title: "Niranjan Fartare | Portfolio",
+    description:
+      "Upcoming SDE. Highly motivated individual and a quick learner. Currently learning Java. Aspiring to be a Full Stack Developer.",
     url: baseUrl,
-    siteName: 'Niranjan Fartare | Portfolio',
-    locale: 'en_US',
-    type: 'website',
-    images :'/images/favicon.ico'
+    siteName: "Niranjan Fartare | Portfolio",
+    locale: "en_US",
+    type: "website",
+    images: "/images/favicon.ico",
   },
   robots: {
     index: true,
@@ -31,27 +32,27 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
-}
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        "text-black bg-white dark:text-white dark:bg-black",
         GeistSans.variable,
-        GeistMono.variable
+        GeistMono.variable,
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
@@ -63,7 +64,6 @@ export default function RootLayout({
           <SpeedInsights />
         </main>
       </body>
-      <GoogleAdsScript />
     </html>
-  )
+  );
 }
