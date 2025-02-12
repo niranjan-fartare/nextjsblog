54c704699e9a2d44142c04983a3547f3c7d6e02c
Niranjan Fartare <niranjanfartare0@gmail.com>
2025-02-12 13:14:51 +0530

    Create GoogleAdsScript.tsx

---
+++ app/components/GoogleAdsScript.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+const GoogleAdsScript = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9568947438501064";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null; // This component does not render anything
+};
+
+export default GoogleAdsScript;
